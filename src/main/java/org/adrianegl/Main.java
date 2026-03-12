c9a4327e3c6d3ed25ba8a77ea94fff127091c06c
Adrians29 <adrian.egl@hotmail.com>
2026-03-12 10:52:52 -0400

    Adding restrictions to the window

--- src/main/java/org/adrianegl/Main.java
+++ src/main/java/org/adrianegl/Main.java
@@ -5,5 +5,11 @@ import javax.swing.*;
 public class Main {
     public static void main(String args[]) {
         JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Mob Psycho");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }

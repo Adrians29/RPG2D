325655ab084c22af9842bc7c75f7569fe6f4da8f
Adrians29 <adrian.egl@hotmail.com>
2026-03-13 10:40:22 -0400

    Adding screen settings to GamePanel class

--- src/main/java/org/adrianegl/GamePanel.java
+++ src/main/java/org/adrianegl/GamePanel.java
@@ -5,4 +5,11 @@ import javax.swing.*;
 public class GamePanel extends JPanel {
     // SCREEN SETTINGS
     final int originalTitleSize = 16; // 16x16 title
+    final int scale = 3;
+
+    final int titleSize = originalTitleSize * scale; //48x48 title
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = titleSize * maxScreenCol; // 768 pixels
+    final int screenHeight = titleSize * maxScreenRow; // 576 pixels
 }

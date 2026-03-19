17f25808a1df502863009516f38d7b692c701e8a
Adrians29 <adrian.egl@hotmail.com>
2026-03-19 09:06:47 -0400

    Creating new variables for the new images and the previous counters

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -7,6 +7,9 @@ public class Entity {
     public int x, y;
     public int speed;
 
-    public BufferedImage center, down1, down2, up1, up2, left1, left2, right1, right2;
+    public BufferedImage down1, center, down2, up1, up, up2, left1, left, left2, right1, right, right2;
     public String direction;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
